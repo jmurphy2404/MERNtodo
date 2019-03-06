9c792a9776ea901f1982ef8d410ea1c710999953
John Murphy <murf2073@gmail.com>
2019-03-05 22:13:35 -0500

    config for connect to mongoDB cluster

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+//connect to the database
+mongoose.connect(process.env.DB, { useNewUrlParser: true })
+  .then(() => console.log(`Database connected successfully`))
+  .catch(err => console.log(err));
+
+mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -11,8 +22,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+
 app.use((req, res, next) => {
-  res.send('Welcome to Express');
+  console.log(err);
+  next();
 });
 
 app.listen(port, () => {
